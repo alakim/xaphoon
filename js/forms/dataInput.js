47ac56f2d0cbb0f5ee10abaad15201d84c4ca08f
alvakim <alvakim@gmail.com>
2014-10-23 11:39:04 +0000

    Диалог ввода данных персоналий

--- js/forms/dataInput.js
+++ js/forms/dataInput.js
@@ -18,9 +18,52 @@
 			var org = db.getOrganization(orgID);
 			return org?div(
 				h2(org.name),
-				apply(org.xmlchildren, function(el){
-					return el.xmltype=="person"?div({"class":"link personEditLink", prsID:el.id}, el.fio):null;
-				})
+				div({"class":"personList", style:"float:left;"},
+					apply(org.xmlchildren, function(el){
+						return el.xmltype=="person"?div({"class":"link personEditLink", prsID:el.id}, el.fio):null;
+					})
+				),
+				div({"class":"personDialog", style:"margin-left:300px; display:none;"},
+					table({border:0, cellpadding:3, cellspacing:0},
+						tr(td("ФИО"),td(
+							input({type:"text", "data-bind":"value:$fio"}),
+							util.validMsg("$fio")
+						)),
+						tr(td("Должность"),td(
+							input({type:"text", "data-bind":"value:$post"}),
+							util.validMsg("$post")
+						)),
+						tr(td("Внутренний телефон"),td(
+							input({type:"text", "data-bind":"value:$inPhone"}),
+							util.validMsg("$inPhone")
+						)),
+						tr(td("Рабочий телефон"),td(
+							input({type:"text", "data-bind":"value:$workPhone"}),
+							util.validMsg("$workPhone")
+						)),
+						tr(td("Факс"),td(
+							input({type:"text", "data-bind":"value:$fax"}),
+							util.validMsg("$fax")
+						)),
+						tr(td("Мобильный телефон"),td(
+							input({type:"text", "data-bind":"value:$mobPhone"}),
+							util.validMsg("$mobPhone")
+						)),
+						tr(td("Электронный адрес"),td(
+							input({type:"text", "data-bind":"value:$email"}),
+							util.validMsg("$email")
+						)),
+						tr(td("Номер комнаты"),td(
+							input({type:"text", "data-bind":"value:$roomNr"}),
+							util.validMsg("$roomNr")
+						)),
+						tr(td("Почтовый адрес"),td(
+							input({type:"text", "data-bind":"value:$address"}),
+							util.validMsg("$address")
+						))
+					),
+					input({type:"button", "data-bind":"click:submitData", value:"Ввод"})
+				)
 			):null;
 		}}
 	};
@@ -30,38 +73,75 @@
 	function viewEditPanel(){
 		var orgID = $("#out .selOrg").val();
 		$("#out .editPnl").html(templates.editPanel(orgID));
+		var model = new PersonModel();
 		$("#out .editPnl .personEditLink").click(function(){
 			var prsID = $(this).attr("prsID");
-			alert(prsID);
+			var person = db.getPerson(prsID);
+			model.openDialog(person);
+			$(".personDialog").show();
 		});
+		
+		ko.applyBindings(model, pnl.find(".personDialog")[0]);
 	}
 	
-	// var organizationIndex = (function(){
-	// 	var index = {};
-	// 	return {
-	// 		build:function(){
-	// 			function indexOrg(org){
-	// 				index[org.id] = org;
-	// 				if(org.xmlchildren){
-	// 					for(var i=0,el,c=org.xmlchildren; el=c[i],i<c.length; i++){
-	// 						if(el.xmltype=="organization") indexOrg(el);
-	// 					}
-	// 				}
-	// 			}
-	// 			$.each($XMLDB.organizations, function(i, org){
-	// 				indexOrg(org);
-	// 			});
-	// 		},
-	// 		getItem: function(orgID){
-	// 			return index[orgID];
-	// 		}
-	// 	};
-	// })();
-	
-	function PersonModel(data){var _=this;
+	var mapping = (function(){
+		var map2Json = {
+			$fio:"fio",
+			$post:"dolzh",
+			$inPhone:"vnutTel",
+			$workPhone:"rabTel",
+			$fax:"faks",
+			$mobPhone:"mobTel",
+			$email:"elekAdr",
+			$roomNr:"numKomn",
+			$address:"potchtAdr",
+		};
+		
+		map2field = {};
+		for(var k in map2Json){var v=map2Json[k]; map2field[v]=k;}
+		
+		return {
+			getField: function(attrNm){return map2field[attNm];},
+			getJsonAttr: function(fieldNm){return map2Json[fieldNm];}
+		};
+	})();
+
+	function PersonModel(){var _=this;
 		$.extend(_, {
+			$fio:ko.observable("").extend({required:"Укажите ФИО"}),
+			$post:ko.observable("").extend({required:"Укажите должность"}),
+			$inPhone:ko.observable(""), //.extend({required:"Укажите внутренний телефон"}),
+			$workPhone:ko.observable(""), //.extend({required:"Укажите рабочий телефон"}),
+			$fax:ko.observable(""), //.extend({required:"Укажите факс"}),
+			$mobPhone:ko.observable(""), //.extend({required:"Укажите мобильный телефон"}),
+			$email:ko.observable("").extend({required:"Укажите электронный адрес"}),
+			$roomNr:ko.observable("").extend({required:"Укажите номер комнаты"}),
+			$address:ko.observable("")//.extend({required:"Укажите почтовый адрес"})
+		});
+		
+		$.extend(_, {
+			openDialog: function(person){var _=this;
+				for(var k in _){
+					if(k.slice(0,1)=="$"){
+						var val = person?person[mapping.getJsonAttr(k)]:"";
+						_[k](val);
+					}
+				}
+			},
+			submitData: function(){
+				var res = {};
+				for(var k in _){
+					if(k.slice(0,1)=="$"){
+						var attNm = mapping.getJsonAttr(k);
+						var val = _[k]();
+						res[attNm] = val==null?"":val;
+					}
+				}
+				console.log(res);
+			}
 		});
 	}
+	
 	
 	return {
 		view: function(){
@@ -74,26 +154,11 @@
 					pnl.html(templates.main(resp));
 					viewEditPanel();
 					$("#out .selOrg").change(function(){
+						$("#out .editPnl").html("");
 						viewEditPanel();
 					});
 				});
 			});
-
-			// if(!$XMLDB){
-			// 	$.post("ws/phonebook.php", {}, function(resp){
-			// 		var data = JSON.parse(resp);
-			// 		$XMLDB = data;
-			// 		// organizationIndex.build();
-			// 		$.post("ws/userPermissions.php", {ticket:ticket}, function(resp){
-			// 			resp = JSON.parse(resp);
-			// 			pnl.html(templates.main(resp));
-			// 			viewEditPanel();
-			// 			$("#out .selOrg").change(function(){
-			// 				viewEditPanel();
-			// 			});
-			// 		});
-			// 	});
-			// }
 		}
 	};
 });
