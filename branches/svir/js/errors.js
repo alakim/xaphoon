cce938b56a2e7d7375563a75eadf4b9fa8a41d02
alvakim@gmail.com <alvakim@gmail.com@4d1e9a17-5d09-95ba-5ebd-432c36e8622e>
2014-11-21 12:43:23 +0000

    Реализованы все операции редактирования дерева организаций

--- branches/svir/js/errors.js
+++ branches/svir/js/errors.js
@@ -2,7 +2,8 @@
 	return {
 		code:{
 			errAuthorizationRequired:"Требуется авторизация",
-			errUserListError:"Ошибка загрузки списка пользователей"
+			errUserListError:"Ошибка загрузки списка пользователей",
+			orgDoesNotExists:"Организация с таким идентификатором не существует"
 		}
 	};
 });
